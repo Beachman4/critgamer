48cd6b89b6d2060c6b91366159f8f5c3c93f35bb
Beachman4 <beachman4@hotmail.com>
2016-04-17 01:35:56 -0500

    stuff

--- resources/views/twitch.blade.php
+++ resources/views/twitch.blade.php
@@ -2,10 +2,10 @@
 @section('content')
     <div class="row">
         <div class="large-8 columns">
-            <iframe src="http://player.twitch.tv/?channel=critgamer" width="800" height="500" allowfullscreen></iframe>
+            <iframe src="https://player.twitch.tv/?channel=critgamer" width="800" height="500" allowfullscreen></iframe>
         </div>
         <div class="large-4 columns">
-            <iframe frameborder="0" scrolling="no" id="chat_embed" src="http://www.twitch.tv/critgamer/chat" height="500" width="370">
+            <iframe frameborder="0" scrolling="no" id="chat_embed" src="https://www.twitch.tv/critgamer/chat" height="500" width="370">
             </iframe>
         </div>
     </div>
